ddd802aeeef066064d45ce7f58ee0a401c640723
Ravi Netravali <ravinet@mit.edu>
2014-01-01 12:53:15 -0500

    cannot list directory files...wip

--- replayserver.cc
+++ replayserver.cc
@@ -31,10 +31,38 @@ using namespace std;
 Find request in environment variables
 for each request, compare to the stored requests until we find a match
 (can have a function which takes two requests and compares them while ignoring the 
-time-sensitive headers)
+time-sensitive headers and stuff after the ?)
 Then, once we find a match, we will write the response (potentially without the time
 sensitive headers).
 */
+
+void list_files( const string & dir, vector< string > & files )
+{
+    DIR *dp;
+    struct dirent *dirp;
+
+    if( ( dp  = opendir( dir.c_str() ) ) == NULL ) {
+        throw Exception( "opendir" );
+    }
+
+    while ( ( dirp = readdir( dp ) ) != NULL ) {
+        if ( string( dirp->d_name ) != "." and string( dirp->d_name ) != ".." ) {
+            files.push_back( dir + string( dirp->d_name ) );
+        }
+    }
+    SystemCall( "closedir", closedir( dp ) );
+}
+
+bool compare_requests( const string & saved_req, const string & new_req )
+{
+    //cout << "ORIGINAL REQUEST: " << saved_req << "<br />\n" << endl;
+    cout << "NEW REQUEST IN FUNCTION: " << new_req << "<br />\n" << endl;
+
+    if ( new_req == saved_req.substr( 0, saved_req.find( "\r\n" ) + 4 ) ) {
+        return true;
+    } 
+    return false;
+}
 
 int main()
 {
@@ -44,7 +72,26 @@ int main()
         for(char **current = environ; *current; current++) {
             cout << *current << endl << "<br />\n";
         }
-        cout << "THIS IS THE REQUEST_URI: " << getenv( "REQUEST_URI" ) << "<br />\n" << endl; 
+        string request;
+        cout << "THIS IS THE REQUEST_URI: " << getenv( "REQUEST_URI" ) << "<br />\n" << endl;
+        string new_req = string( getenv( "REQUEST_METHOD" ) ) + " " + string( getenv( "SCRIPT_NAME" ) ) + " " + string ( getenv( "SERVER_PROTOCOL" ) ) + "\r\n";
+        cout << "NEW REQUEST: " << new_req << "<br />\n" << endl;
+
+        vector< string > files;
+        list_files( "/home/ravi/mahimahi/blah/", files );
+        for ( unsigned int i = 0; i < files.size(); i++ ) { // iterate through files and call method which compares requests
+            int fd = SystemCall( "open", open( files[i].c_str(), O_RDONLY ) );
+            HTTP_Record::reqrespair current_record;
+            current_record.ParseFromFileDescriptor( fd );
+            HTTP_Record::http_message saved_req = current_record.req();
+            if ( compare_requests( saved_req.first_line(), new_req ) ) { // requests match
+                cout << current_record.res().body() << endl;
+            }
+            cout << "First Line: " << saved_req.first_line() << "DONE" << endl;
+            cout << "HEADER: " << saved_req.headers(1) << "DONE" << endl;
+            cout << "BODY: " << saved_req.body() << "DONE" << endl;
+            SystemCall( "close", close( fd ) );
+        }
     } catch ( const Exception & e ) {
         e.perror();
         return EXIT_FAILURE;
