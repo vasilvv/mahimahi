a07db6fd77ca8672cd48fb84cb95a7c2cd844f4d
Ravi Netravali <ravinet@mit.edu>
2014-01-04 09:15:37 -0500

    use vectors in longest_substr

--- replayserver.cc
+++ replayserver.cc
@@ -93,37 +93,37 @@ bool compare_requests( HTTP_Record::reqrespair & saved_record, vector< HTTP_Reco
 
 int longest_substr( const string & str1, const string & str2 )
 {
-     if ( str1.empty() || str2.empty() ) {
-         return 0;
-     }
+    /* http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring#C.2B.2B */
 
-     int *curr = new int [ str2.size() ];
-     int *prev = new int [ str2.size() ];
-     int *swap = nullptr;
-     int maxSubstr = 0;
+    if ( str1.empty() || str2.empty() ) {
+        return 0;
+    }
 
-     for ( unsigned int i = 0; i < str1.size(); i++ ) {
-         for ( unsigned int j = 0; j < str2.size(); j++) {
-               if ( str1[ i ] != str2[ j ] ) {
-                   curr[ j ] = 0;
-               } else {
-                   if ( i == 0 || j == 0 ) {
-                       curr[ j ] = 1;
-                   } else {
-                       curr[ j ] = 1 + prev[ j-1 ];
-                   }
-                   if( maxSubstr < curr[ j ] ) {
-                       maxSubstr = curr[ j ];
-                   }
-               }
-         }
-         swap=curr;
-         curr=prev;
-         prev=swap;
-     }
-     delete [] curr;
-     delete [] prev;
-     return maxSubstr;
+    vector< int > curr (str2.size() );
+    vector< int > prev (str2.size() );
+    vector< int > swap;
+    int maxSubstr = 0;
+
+    for ( unsigned int i = 0; i < str1.size(); i++ ) {
+        for ( unsigned int j = 0; j < str2.size(); j++) {
+            if ( str1[ i ] != str2[ j ] ) {
+                curr.at( j ) = 0;
+            } else {
+                if ( i == 0 || j == 0 ) {
+                    curr.at( j ) = 1;
+                } else {
+                    curr.at( j ) = 1 + prev.at( j-1 );
+                }
+                if( maxSubstr < curr.at( j ) ) {
+                    maxSubstr = curr.at( j );
+                }
+            }
+        }
+        swap=curr;
+        curr=prev;
+        prev=swap;
+    }
+    return maxSubstr;
 }
 
 int closest_match( const vector< HTTP_Record::reqrespair > & possible )
@@ -186,7 +186,7 @@ int main()
                 return EXIT_FAILURE;
             } else { /* for now, return first stored possibility */
                 return_message( possible_matches.at( closest_match( possible_matches ) ) );
-                throw Exception( "replayserver", "Had to choose randomly for: " + string( getenv( "REQUEST_URI" ) ) );
+                throw Exception( "replayserver", "Chose longest query string match for: " + string( getenv( "REQUEST_URI" ) ) );
             }
         }
     } catch ( const Exception & e ) {
