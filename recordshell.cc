55dcd1cf1be05546f3e34a322cdcbfceb58d345b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-01-04 15:57:59 -0500

    Store protobufs with the correct permissions, closes #41

--- recordshell.cc
+++ recordshell.cc
@@ -23,8 +23,9 @@ using namespace std;
 using namespace PollerShortNames;
 
 int eventloop( unique_ptr<DNSProxy> && dns_proxy,
-               ChildProcess & child_process,
-               unique_ptr<HTTPProxy> && http_proxy );
+               unique_ptr<HTTPProxy> && http_proxy,
+               unique_ptr<ChildProcess> && child1,
+               unique_ptr<ChildProcess> && child2 );
 
 int main( int argc, char *argv[] )
 {
@@ -39,8 +40,11 @@ int main( int argc, char *argv[] )
             throw Exception( "Usage", string( argv[ 0 ] ) + " folder_for_recorded_content" );
         }
 
-        /* check if user-specified storage folder exists, and if not, create it */
-        string directory = check_storage_folder( argv[1] );
+        /* Make sure directory ends with '/' so we can prepend directory to file name for storage */
+        string directory( argv[ 1 ] );
+        if ( directory.back() != '/' ) {
+            directory.append( "/" );
+        }
 
         const Address nameserver = first_nameserver();
 
@@ -72,7 +76,7 @@ int main( int argc, char *argv[] )
         DNAT dnat( http_proxy->tcp_listener().local_addr(), egress_name );
 
         /* Fork */
-        ChildProcess container_process( [&]() {
+        unique_ptr<ChildProcess> container_process( new ChildProcess( [&]() {
                 /* bring up localhost */
                 interface_ioctl( Socket( UDP ).fd(), SIOCSIFFLAGS, "lo",
                                  [] ( ifreq &ifr ) { ifr.ifr_flags = IFF_UP; } );
@@ -85,7 +89,7 @@ int main( int argc, char *argv[] )
                 /* Fork again after dropping root privileges */
                 drop_privileges();
 
-                ChildProcess bash_process( [&]() {
+                unique_ptr<ChildProcess> bash_process( new ChildProcess( [&]() {
                         /* restore environment and tweak bash prompt */
                         environ = user_environment;
                         prepend_shell_prefix( "[record] " );
@@ -93,16 +97,16 @@ int main( int argc, char *argv[] )
                         const string shell = shell_path();
                         SystemCall( "execl", execl( shell.c_str(), shell.c_str(), static_cast<char *>( nullptr ) ) );
                         return EXIT_FAILURE;
-                    } );
+                    } ) );
 
-                return eventloop( move( dns_inside ), bash_process, nullptr );
-            }, true ); /* new network namespace */
+                return eventloop( move( dns_inside ), nullptr, move( bash_process ), nullptr );
+            }, true ) ); /* new network namespace */
 
         /* give ingress to container */
-        run( { IP, "link", "set", "dev", ingress_name, "netns", to_string( container_process.pid() ) } );
+        run( { IP, "link", "set", "dev", ingress_name, "netns", to_string( container_process->pid() ) } );
 
         /* bring up ingress */
-        in_network_namespace( container_process.pid(), [&] () {
+        in_network_namespace( container_process->pid(), [&] () {
                 /* bring up veth device */
                 assign_address( ingress_name, ingress_addr, egress_addr );
 
@@ -116,8 +120,14 @@ int main( int argc, char *argv[] )
 
                 SystemCall( "ioctl SIOCADDRT", ioctl( Socket( UDP ).fd().num(), SIOCADDRT, &route ) );
             } );
+        unique_ptr<ChildProcess> recordr_process( new ChildProcess( [&]() {
+                drop_privileges();
+                /* check if user-specified storage folder exists, and if not, create it */
+                check_storage_folder( directory );
+                return eventloop( move( dns_outside ), move( http_proxy ), nullptr, nullptr );
+        } ) );
 
-        return eventloop( move( dns_outside ), container_process, move( http_proxy ) );
+        return eventloop( nullptr, nullptr, move( container_process ), move( recordr_process ) );
     } catch ( const Exception & e ) {
         e.perror();
         return EXIT_FAILURE;
@@ -127,8 +137,9 @@ int main( int argc, char *argv[] )
 }
 
 int eventloop( unique_ptr<DNSProxy> && dns_proxy,
-               ChildProcess & child_process,
-               unique_ptr<HTTPProxy> && http_proxy )
+               unique_ptr<HTTPProxy> && http_proxy,
+               unique_ptr<ChildProcess> && child1,
+               unique_ptr<ChildProcess> && child2 )
 {
     /* set up signal file descriptor */
     SignalMask signals_to_listen_for = { SIGCHLD, SIGCONT, SIGHUP, SIGTERM };
@@ -160,16 +171,30 @@ int eventloop( unique_ptr<DNSProxy> && dns_proxy,
                                            } ) );
     }
 
-    /* we get signal -> main screen turn on -> handle signal */
-    poller.add_action( Poller::Action( signal_fd.fd(), Direction::In,
-                                       [&] () {
-                                           return handle_signal( signal_fd.read_signal(),
-                                                                 child_process );
-                                       } ) );
+    if ( child1 ) {
+        /* we got a signal from child1 -> handle signal */
+        poller.add_action( Poller::Action( signal_fd.fd(), Direction::In,
+                                           [&] () {
+                                               return handle_signal( signal_fd.read_signal(),
+                                                                     *child1 );
+                                           } ) );
+    }
+
+    if ( child2 ) {
+        /* we got a signal from child2 -> handle signal */
+        poller.add_action( Poller::Action( signal_fd.fd(), Direction::In,
+                                           [&] () {
+                                               return handle_signal( signal_fd.read_signal(),
+                                                                     *child2 );
+                                           } ) );
+    }
 
     while ( true ) {
         auto poll_result = poller.poll( 60000 );
         if ( poll_result.result == Poller::Result::Type::Exit ) {
+            /* If we are exiting, kill any (possibly alive) children */
+            if ( child1 ) child1->signal( SIGKILL );
+            if ( child2 ) child2->signal( SIGKILL );
             return poll_result.exit_status;
         }
     }
