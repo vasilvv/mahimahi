78c6c4a94c66c6b289c6c18fa5b303244a0a6174
Ravi Netravali <ravinet@mit.edu>
2014-01-19 20:24:25 -0500

    archive class with helper functions for http_proxy and bulk parser

--- archive.hh
+++ archive.hh
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <utility>
 
 #include "http_record.pb.h"
 
@@ -15,6 +16,8 @@ class Archive
 private:
     std::vector< std::pair< HTTP_Record::http_message, std::string > > pending_ {};
 
+    std::string get_corresponding_response( const HTTP_Record::http_message & new_req );
+
 public:
     Archive() {};
 
@@ -22,7 +25,7 @@ public:
     void add_request( const HTTP_Record::http_message & request );
 
     /* Add a response */
-    void add_response( const std::string & response, const int position );
+    void add_response( const std::string & response, const size_t position );
 
     /* Do we have a matching request that is pending? */
     bool is_request_pending( const HTTP_Record::http_message & new_req );
@@ -31,7 +34,7 @@ public:
     bool have_response( const HTTP_Record::http_message & new_req );
 
     /* Return the corresponding response to the stored request (caller should first call have_response) */ 
-    HTTP_Record::http_message corresponding_response( const HTTP_Record::http_message & new_req );
+    std::string corresponding_response( const HTTP_Record::http_message & new_req );
 };
 
 #endif
